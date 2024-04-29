71a9d775b95868ab48e2c4585c46500911f04299
SHZTD <tdrag@elpuig.xeill.net>
2024-04-29 16:56:32 +0200

    I n i t i a l  c o m m i t.

---
+++ src/test/java/MatrixTest.java
@@ -0,0 +1,58 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MatrixTest {
+
+    @org.junit.jupiter.api.Test
+    void getWidth() {
+        int width = 5;
+        int height = 5;
+        Matrix m = new Matrix(width, height);
+        Assertions.assertEquals(width, m.getWidth());
+    }
+
+    /* ---- */
+
+    @ParameterizedTest
+    @CsvSource({"5,5", "10,20", "9384,12384"})
+    void getWidth_OK(int width, int height) {
+        Matrix m = new Matrix(width, height);
+        Assertions.assertEquals(width, m.getWidth());
+    }
+
+    @org.junit.jupiter.api.Test
+    void getWidth_NEGATIVE() {
+        int width = -1;
+        int height = 5;
+        Assertions.assertThrows(NegativeArraySizeException.class, () -> {
+            new Matrix(width, height);
+        });
+    }
+
+    /* ---- */
+
+    @Test
+    void getHeight() {
+    }
+
+    @Test
+    void fill() {
+    }
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void found() {
+    }
+
+    @Test
+    void ToString() {
+    }
+}
